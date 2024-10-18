cf6276b76c4c2cd27a109176e0fb044f978d2b58
Allwin E K <ekallwin@gmail.com>
2024-10-18 21:25:15 +0530

    First Commit

---
+++ functions.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const upArrow = document.getElementById("upArrow");
+    
+    // Add a scroll event listener
+    window.addEventListener("scroll", function() {
+    if (window.scrollY > 100) { 
+        upArrow.style.display = "block"; 
+    } else {
+        upArrow.style.display = "none"; 
+    }
+    });
+    
+    upArrow.addEventListener("click", function() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+    });
+    
+    });
